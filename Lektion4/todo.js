8ed40ce962b4f0ce47a137e208cb9365298e706c
zushii <zxgamingh@gmail.com>
2024-09-05 13:24:15 +0200

    Added remove button

--- Lektion4/todo.js
+++ Lektion4/todo.js
@@ -6,6 +6,7 @@ function addTask(){
 
   const newTask = document.createElement("li");
   newTask.classList.add("list-style");
+  newTask.id=`task${taskCounter}`;
   document.getElementById("task-list").appendChild(newTask);
 
   const textField = document.createElement("input");
@@ -13,8 +14,24 @@ function addTask(){
   textField.value = "Write your task here!";
   newTask.appendChild(textField);
 
+  textField.addEventListener("focus", function() {
+    if (this.value === "Write your task here!") {
+        this.value = "";
+    }
+  });
+
   const checkBox = document.createElement("input");
   checkBox.type = "checkbox";
   checkBox.id=`checkbox${taskCounter}`;
   newTask.appendChild(checkBox)
+
+  const delButton = document.createElement("button");
+  delButton.innerText = "x";
+  newTask.appendChild(delButton);
+  
+}
+
+function removeTask(taskId){
+  const removedTask = document.getElementById(taskId);
+  removedTask.remove();
 }
