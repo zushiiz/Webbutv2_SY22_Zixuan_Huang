6c33f4614415acf8be548f471ec9090aeeb230dc
zushii <zxgamingh@gmail.com>
2024-12-09 12:11:15 +0100

    404 page done

--- Bootstrap - Zixuan Huang/app.js
+++ Bootstrap - Zixuan Huang/app.js
@@ -2,3 +2,20 @@ function scrollToArticle(a) {
   const element = document.getElementById(a);
   element.scrollIntoView({ behavior: "smooth", block: "start" });
 }
+
+const element = document.getElementById('raccoon');
+const button = document.getElementById('toggleButton');
+
+button.addEventListener('click', () => {
+  if (element.classList.contains('spin1')) {
+    element.classList.remove('spin1');
+    element.classList.add('spin2');
+  } else if (element.classList.contains('spin2')) {
+    element.classList.remove('spin2');
+    element.classList.add('spin3');
+  }
+  else{
+    element.classList.remove('spin3');
+    element.classList.add('spin1');
+  }
+});
